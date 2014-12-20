d9c15c6063f2c57fa70030fa196e1b7f8e39a30c
timakin <timaki.st@gmail.com>
2014-12-20 16:32:56 +0900

    site cookbooks addition

---
+++ centOS/vendor/chef-repo/site-cookbooks/neobundle/recipes/default.rb
@@ -0,0 +1,32 @@
+#
+# Cookbook Name:: neobundle
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+
+directory "home/vagrant/.vim" do
+  owner 'root'
+  group 'root'
+  mode '0777'
+  action :create
+end
+
+
+directory "home/vagrant/.vim/bundle" do
+  owner 'root'
+  group 'root'
+  mode '0777'
+  action :create
+end
+
+git "home/vagrant/.vim/bundle/neobundle.vim" do
+  repository "git@github.com:Shougo/neobundle.vim.git"
+  reference "master"
+  action :checkout
+  user "vagrant"
+  group "vagrant"
+end
