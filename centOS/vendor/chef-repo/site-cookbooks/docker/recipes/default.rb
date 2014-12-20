328b897bd5f3223f9e7067b785e94053b0bb1b91
timakin <timaki.st@gmail.com>
2014-12-21 02:20:29 +0900

    docker

---
+++ centOS/vendor/chef-repo/site-cookbooks/docker/recipes/default.rb
@@ -0,0 +1,19 @@
+#
+# Cookbook Name:: docker
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+yum_package "docker-io" do
+  action :install
+end
+
+bash "docker installation" do
+  code <<-EOT
+    sudo service docker start
+    sudo chkconfig docker on
+    sudo docker pull ubuntu
+  EOT
+end
