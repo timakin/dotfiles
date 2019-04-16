276638bce8c6c77f00fe9c11cbce42350cb4913c
timakin <timaki.st@gmail.com>
2019-04-16 10:22:15 +0900

    cnn

--- zsh.d/news.zsh
+++ zsh.d/news.zsh
@@ -2,4 +2,5 @@
 alias stockclip="open https://www.stockclip.net/"
 alias gn="open https://news.google.com"
 alias feedly="open https://feedly.com/"
-alias news="open -n /Applications/Chrome.app/; gn; stockclip; feedly"
+alias cnn="open https://www.cnn.co.jp/"
+alias news="open -n /Applications/Chrome.app/; gn; stockclip; feedly; cnn"
