3a8e5095b5174cc9c5a33e060f117c53aa7fb56f
timakin <timaki.st@gmail.com>
2021-03-03 01:10:20 +0900

    fix: home

--- zsh.d/gcloud.zsh
+++ zsh.d/gcloud.zsh
@@ -1,5 +1,5 @@
 # The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/seiji.takahashi/Downloads/google-cloud-sdk/path.zsh.inc' ]; then . '/Users/seiji.takahashi/Downloads/google-cloud-sdk/path.zsh.inc'; fi
+if [ -f '~/google-cloud-sdk/path.zsh.inc' ]; then . '~/google-cloud-sdk/path.zsh.inc'; fi
 
 # The next line enables shell command completion for gcloud.
-if [ -f '/Users/seiji.takahashi/Downloads/google-cloud-sdk/completion.zsh.inc' ]; then . '/Users/seiji.takahashi/Downloads/google-cloud-sdk/completion.zsh.inc'; fi
+if [ -f '~/google-cloud-sdk/completion.zsh.inc' ]; then . '~/google-cloud-sdk/completion.zsh.inc'; fi
