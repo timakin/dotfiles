fe70036cbc65593d0e9ac6047d4d9a734f9e9b45
timakin <timaki.st@gmail.com>
2021-03-23 18:42:47 +0900

    fix: gcloud

--- zsh.d/path.zsh
+++ zsh.d/path.zsh
@@ -6,9 +6,3 @@ export GOPATH=$HOME/go
 export PATH=$PATH:$GOPATH/bin
 export PATH=$PATH:/bin:/usr/bin:/usr/local/bin
 export PATH=$PATH:$HOME/google-cloud-sdk/bin
-
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '~/google-cloud-sdk/path.zsh.inc' ]; then . '~/google-cloud-sdk/path.zsh.inc'; fi
-
-# The next line enables shell command completion for gcloud.
-if [ -f '~/google-cloud-sdk/completion.zsh.inc' ]; then . '~/google-cloud-sdk/completion.zsh.inc'; fi
