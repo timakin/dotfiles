b987d609b3bccdf0fc1e0afd9a139af843801ea0
timakin <timaki.st@gmail.com>
2022-05-30 15:09:21 +0900

    phpstorm

--- zsh.d/alias.sh
+++ zsh.d/alias.sh
@@ -6,7 +6,7 @@ alias diff='diff --strip-trailing-cr'
 alias less='less -R' # Color escape sequences will displayed
 alias cat='bat'
 alias gcloudtoken='gcloud auth print-identity-token | pbcopy'
-
+pstorm () { open -na "PhpStorm.app" --args "$@" }
 ### replace words under current directory
 function greplace() {
     find . -name ${3:-*} | xargs sed -i "" "s/$1/$2/g"
